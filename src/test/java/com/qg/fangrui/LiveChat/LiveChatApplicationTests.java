09d3d89a7ae9f49962b27c5f4d88e883c451ad32
FunriLy <fr479910770@qq.com>
2017-06-26 22:22:38 +0800

    project initialize

---
+++ src/test/java/com/qg/fangrui/LiveChat/LiveChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qg.fangrui.LiveChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiveChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
